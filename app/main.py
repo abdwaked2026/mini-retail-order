f1a1c985c954047a0c3e83293da3b1c93191d0f5
akw-waked <abd_waked@yahoo.com>
2026-05-24 16:57:44 -0400

    Set up FastAPI backend starter

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Mini Retail Order API")
 
-
+# Test endpoint to verify that the API is running
 @app.get("/")
 def read_root():
     return {"message": "Mini Retail Order API is running"}
